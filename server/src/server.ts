1808b2f9ac83d05fb8a96c51c5bece69a6e6c626
isaiasg09 <isaiashsobral@gmail.com>
2023-01-21 15:42:21 -0300

    fixed backend connection with mobile

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,8 @@ import Fastify from "fastify";
 import cors from "@fastify/cors";
 import { appRoutes } from "./routes";
 
+const localtunnel = require("localtunnel");
+
 const app = Fastify();
 
 app.register(cors);
@@ -9,8 +11,15 @@ app.register(appRoutes);
 
 app
   .listen({
-    port: 4343,
+    host: "0.0.0.0",
+    port: 3333,
   })
-  .then(() => {
-    console.log("HTTP server está rodando poggers");
+  .then(async () => {
+    const tunnel = await localtunnel({
+      local_host: "0.0.0.0",
+      port: 3333,
+      subdomain: "dasmdyucbsy-api",
+    });
+
+    console.log("HTTP Server Running!");
   });
