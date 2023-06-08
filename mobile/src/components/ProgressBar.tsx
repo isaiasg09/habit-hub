e49beafe2716db84cde513c838fef1a63a8f051c
isaiasg09 <isaiashsobral@gmail.com>
2023-06-08 16:33:48 -0300

    mobile: added animation on progress bar with react native reanimated

--- mobile/src/components/ProgressBar.tsx
+++ mobile/src/components/ProgressBar.tsx
@@ -1,16 +1,40 @@
+import Animated, {
+  useSharedValue,
+  withTiming,
+  useAnimatedStyle,
+  Easing,
+} from "react-native-reanimated";
 import { View } from "react-native";
+import { useEffect } from "react";
 
 interface ProgressBarProps {
   progress?: number;
 }
 
 export function ProgressBar({ progress = 0 }) {
+  const sharedProgress = useSharedValue(progress);
+
+  useEffect(() => {
+    sharedProgress.value = withTiming(progress, config);
+  }, [progress]);
+
+  const config = {
+    duration: 300,
+    easing: Easing.bezier(0.5, 0.01, 0, 1),
+  };
+
+  const style = useAnimatedStyle(() => {
+    return {
+      width: `${sharedProgress.value}%`,
+    };
+  });
+
   return (
     <View className="w-full h-3 rounded-xl bg-zinc-700 mt-4">
-      <View
+      <Animated.View
         className="h-3 rounded-xl bg-violet-600 transition-all duration-200"
-        style={{ width: `${progress}%` }}
-      ></View>
+        style={style}
+      />
     </View>
   );
 }
